451d0650112b007f9154bd396eb64221b3393805
Michael <mIm@awesome.com>
2020-06-28 16:08:23 -0400

    check for directory

--- cat/cat.pl
+++ cat/cat.pl
@@ -2,6 +2,11 @@
 
 # cat...now in perl!
 
+if ( -d $ARGV[0] ) {
+    print "$ARGV[0] is a directory.\n";
+    exit;
+}
+
 open(cat_this, '<', $ARGV[0]) or die "$ARGV[0] is not a file...\n";
 
 print "you typed: $myin\n";
